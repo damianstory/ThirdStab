6a04120fde587da0ce0c7ed6730f8f3b7f9d7541
Damian Matheson <damian@example.com>
2025-10-08 17:32:43 -0400

    SCC title change

--- src/data/activities.ts
+++ src/data/activities.ts
@@ -43,7 +43,7 @@ export const activities: Activity[] = [
     id: '2',
     month: 'November',
     year: '2025',
-    title: 'Skills/Compétences Canada',
+    title: 'Skills for Success Challenge',
     description: 'Explore Canada\'s Skills for Success: nine skills that can set workers up for success with work, learning, and life – no matter where they live or what job they pursue.',
     sponsor: {
       name: 'Skills Competences Canada',
